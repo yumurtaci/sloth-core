c9139290bd9d0c3dcc3699e82cd33046bea8216f
Batuhan Yumurtaci <batuhan.yumurtaci@tum.de>
2022-05-04 22:03:56 +0100

    waypoint mission from yaml file and class styling

--- state_machine/include/state_machine/state_machine.h
+++ state_machine/include/state_machine/state_machine.h
@@ -1,26 +1,35 @@
+/**
+ * @file state_machine.h
+ *
+ * @date 03 June 2022
+ * 
+ * @brief State Machine Mavros
+ * 
+ * @author Batuhan Yumurtaci <batuhan.yumurtaci@tum.de>
+ */
+
 #ifndef STATEMACHINE_H
 #define STATEMACHINE_H
 
-#include <mutex>
-#include <thread>
+#include <ros/ros.h>
+#include <stdlib.h>
 
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Geometry>
 
-#include <ros/ros.h>
 #include <mavros_msgs/State.h>
 #include <mavros_msgs/SetMode.h>
-#include <mavros_msgs/ParamSet.h>
-#include <mavros_msgs/ParamGet.h>
 #include <mavros_msgs/CommandTOL.h>
 #include <mavros_msgs/CommandBool.h>
-
 #include <geometry_msgs/PoseStamped.h>
+
+#include <thread>
+#include <mutex>
 
 namespace statemachine
 {
-
+  
   class StateMachine
   {
 
@@ -29,68 +38,85 @@ namespace statemachine
 
     enum States
     {
-        ARMED = 0,
-        TAKEOFF = 1,
-        WAYPOINT = 2,
-        TRAJECTORY = 3,
-        LANDING = 4
+      ARMED = 0,
+      TAKEOFF = 1,
+      WAYPOINT = 2,
+      TRAJECTORY = 3,
+      LANDING = 4,
     };
-
-    //Eigen::Vector3d quad_pos;
+    
+    // Define vectors and matrices
     typedef Eigen::Vector3d Vec3;
     typedef Eigen::Vector4d Vec4;
     typedef Eigen::Matrix<double, 6, 1> StateVectorEuler;
     typedef Eigen::Matrix<double, 7, 1> StateVectorQuaternion;
+    typedef Eigen::Matrix<double, 4, Eigen::Dynamic> WaypointMatrix;
     
   private:
-    // Node handles
-    ros::NodeHandle nh_;
-    // Publishers
-    ros::Publisher local_pos_pub_;              // Desired Position & Oritentation
-    // Subscribers
-    ros::Subscriber state_sub_;                 // FCU state
+    bool wp_completed_;                         // Flag used for waypoint mission
+    bool request_input_ = false;                // Keyboard input trigger
+
+    int n_wp_;                                  // Total number of waypoints
+    int state_;                                 // State of the state machine
+    int input_;                                 // Keyboard input
+    int current_wp_;                            // Current waypoint number
+
+    ros::Rate rate_ = 20;                       // Loop rate
+    
+    ros::NodeHandle nh_;                        // Node handles
+    
+    ros::Publisher local_pos_pub_;              // Desired Position & Orientation publisher
+    
+    ros::Subscriber state_sub_;                 // FCU state subscriber
     ros::Subscriber local_pos_pose_sub_;        // Position & Orientation 
-    // Services
+
     ros::ServiceClient arming_client_;          // Arming ros service
     ros::ServiceClient set_mode_client_;        // Setting mode ros service
-    ros::ServiceClient land_client_;            // Sanding ros service
-    
-    // Parameters
-    int state_;                                 // State of the state machine
-    const int rcl_except_ = 4;                  // Radio setting for offboard mode
-    int input_;                                 // Keyboard input
-    bool request_input_ = false;                // Keyboard input trigger
-    std::mutex input_mutex_;                    // Non blocking state machine
-
-    // Memory for sharing information between functions
-    //Eigen::Vector3d quad_pos_;
-    //Eigen::Vector3d goal_pos_;
-    //Eigen::Vector3d quad_vel_;
-    //Eigen::Matrix3d quad_rot_;
-    //Eigen::Vector3d quad_rate_;
+    ros::ServiceClient land_client_;            // Landing ros service
 
     mavros_msgs::State current_state_;          // FCU state
     mavros_msgs::SetMode offb_mode_cmd_;        // Offboard command for mode service
     mavros_msgs::CommandBool arm_cmd_;          // Arm command for arming service
-    mavros_msgs::CommandBool disarm_cmd_;       // Disarm command for arming service
     mavros_msgs::CommandTOL land_cmd_;          // Land command for landing service
 
     geometry_msgs::PoseStamped des_pose_;       // Target pose
     geometry_msgs::PoseStamped takeoff_pose_;   // Take-off pose
     geometry_msgs::PoseStamped localtakeoff_pose_;   // Local Take-off pose
     geometry_msgs::PoseStamped current_pose_;   // Current pose 
+
+    WaypointMatrix wp_matrix_;                  // Matrix to store Waypoints
+
+    std::mutex input_mutex_;                    // Non blocking state machine
     
-    // Callback Functions
+    // Callback function to get the current fcu status
     void stateCallback(const mavros_msgs::State::ConstPtr &msg);
+    
+    // Callback function to get the current pose
     void localposeCallback(const geometry_msgs::PoseStamped::ConstPtr &msg);
     
-    // Functions
-    void UpdateState();                         // Update the states of the state machine
-    void PublishCommand();                      // Commands in a particular state
+    // Transition conditions of the state machine
+    void UpdateState();     
     
-    //bool WPConvergence(Vec3 quad_pos1, Vec3 quad_pos2); // Check if arrived at target waypoint
-    bool WPConvergence(geometry_msgs::PoseStamped pose1, geometry_msgs::PoseStamped pose2); // Check if arrived at target waypoint
+    // Commands in a particular state
+    void PublishCommand();
     
+    // Read the waypoints from YAML file
+    void ReadWaypoints();
+    
+    // Initialize the FCU connection for Mavros and PX4
+    void InitializeFCU();
+    
+    // Initialize publishers, subscribers and servies
+    void InitializePublishers();
+    void InitializeSubscribers();
+    void InitializeServices();  
+    
+    // Check if arrived at target waypoint
+    bool WPConvergence(geometry_msgs::PoseStamped pose1, geometry_msgs::PoseStamped pose2); 
+    
+    // Decouple threads of UpdateState and PublishCommand as waiting for user input
+    // Required for uniterrupted advertisement of the desired pose 
+    // Otherwise the offboard mode is deactivated
     int getInput();
     void getKeyboardInput();
     void setInput(int input);
--- state_machine/src/state_machine.cpp
+++ state_machine/src/state_machine.cpp
@@ -1,114 +1,75 @@
+/**
+ * @file state_machine.cpp
+ *
+ * @date 03 June 2022
+ * 
+ * @brief State Machine Mavros
+ * 
+ * @author Batuhan Yumurtaci <batuhan.yumurtaci@tum.de>
+ */
+
 #include <state_machine/state_machine.h>
 
-#define TAKEOFF_ALTITUDE 1.5f
+#define FCU_TIME_BUFFER 3.0f
 
-// TODO: Refactorize WPConvergence value
-// TODO: Add heading command and convergence checks
-// TODO: YAML file to read the waypoints
-// TODO: Geometric controller activation
+// TODO : Refactorize WPConvergence value with orientation and velocity
+// TODO : Add heading for waypoints and convergence checks
+// TODO : Geometric controller activation
+// TODO : Trajectory state
 
 namespace statemachine
 {
-
   StateMachine::StateMachine(ros::NodeHandle nh) : nh_(nh)
   {
-    ROS_INFO("Started State Machine");
-    ros::Rate rate(100);
+    ROS_INFO("[INIT]: Started State Machine");
 
-    // Publishers
-    local_pos_pub_ = nh_.advertise<geometry_msgs::PoseStamped>
-            ("mavros/setpoint_position/local", 1);
-
-    // Subscribers
-    state_sub_ = nh_.subscribe<mavros_msgs::State>
-            ("mavros/state", 10, &StateMachine::stateCallback, this);
-    local_pos_pose_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>
-            ("mavros/local_position/pose", 10, &StateMachine::localposeCallback, this);
-        
-    // Services
-    arming_client_ = nh_.serviceClient<mavros_msgs::CommandBool>
-            ("mavros/cmd/arming");
-    set_mode_client_ = nh_.serviceClient<mavros_msgs::SetMode>
-            ("mavros/set_mode");
-    land_client_ = nh.serviceClient<mavros_msgs::CommandTOL>
-            ("mavros/cmd/land");
-    
-    // Initialize the variables
-    state_ = ARMED; 
+    InitializePublishers();
+    InitializeSubscribers();
+    InitializeServices();
 
     // Commands for ros services
     offb_mode_cmd_.request.custom_mode = "OFFBOARD";
     arm_cmd_.request.value = true;
-    disarm_cmd_.request.value = false;
     land_cmd_.request.yaw = 0;
     land_cmd_.request.latitude = 0;
     land_cmd_.request.longitude = 0;
     land_cmd_.request.altitude = 0;
+
+    // Initialize the variables
+    state_ = ARMED;
+
+    n_wp_ = 0;
+    current_wp_ = 0;
+    wp_completed_ = false;
 
     // ENU frame is used -> PX4 transforms to NED
     des_pose_.pose.position.x = 0;
     des_pose_.pose.position.y = 0;
     des_pose_.pose.position.z = 0;
 
-    takeoff_pose_.pose.position.x = 0;
-    takeoff_pose_.pose.position.y = 0;
-    takeoff_pose_.pose.position.z = TAKEOFF_ALTITUDE;
-
-    // FCU Initialisation Sequence 
-    ros::Time last_request = ros::Time::now();
-
-    // Wait for FCU connection before publishing anything
-    while(ros::ok() && !current_state_.connected && 
-    (ros::Time::now() - last_request > ros::Duration(5.0))){
-        ros::spinOnce();
-        rate.sleep();
-        ROS_INFO("[INIT]: Connecting to FCT...");
-    }
+    // Read the take-off pose
+    nh_.getParam(ros::this_node::getName() + "/takeoff/x", takeoff_pose_.pose.position.x);
+    nh_.getParam(ros::this_node::getName() + "/takeoff/y", takeoff_pose_.pose.position.y);
+    nh_.getParam(ros::this_node::getName() + "/takeoff/z", takeoff_pose_.pose.position.z);
     
-    // Send a few setpoints before starting, 
-    // Otherwise cant switch to Offboard
-    for(int i = 100; ros::ok() && i > 0; --i){
-        local_pos_pub_.publish(des_pose_);
-        ros::spinOnce();
-        rate.sleep();
-    }
+    ReadWaypoints();
+    InitializeFCU();                   
+    
+    // Start state machine
+    if (current_state_.armed){
+      if(getInput() == 0) 
+          state_ = ARMED;
+      requestKeyboardInput();
+    } else {
+      setInput(100);
+      }  
 
-    // Timer used to prevent FCU overflowing
-    //ros::Time last_request = ros::Time::now();
-    last_request = ros::Time::now();
-        
-    local_pos_pub_.publish(des_pose_);
-
-    // Change to Offboard mode and arm w. 5 second gaps
-    while(ros::ok() && !current_state_.armed){
-        if( current_state_.mode != "OFFBOARD" && 
-        (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client_.call(offb_mode_cmd_) && offb_mode_cmd_.response.mode_sent){
-                ROS_INFO("[INIT]: Offboard enabled");
-            }
-            last_request = ros::Time::now();
-        } else {
-            if( current_state_.mode == "OFFBOARD" && !current_state_.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client_.call(arm_cmd_) &&
-                    arm_cmd_.response.success){
-                    ROS_INFO("[INIT]: Vehicle armed");
-                }
-                last_request = ros::Time::now();
-            }
-        }
-        local_pos_pub_.publish(des_pose_);
-        ros::spinOnce();
-        rate.sleep();
-    }
-
-    // State Machine
     while (ros::ok())
     {
       ros::spinOnce();
       UpdateState();
       PublishCommand();
-      rate.sleep();
+      rate_.sleep();
     }
 
   }
@@ -117,12 +78,11 @@ namespace statemachine
   void StateMachine::stateCallback(const mavros_msgs::State::ConstPtr &msg)
   {
     this->current_state_ = *msg;
-
     }
-
+  
   void StateMachine::localposeCallback(const geometry_msgs::PoseStamped::ConstPtr &msg)
   {
-      this->current_pose_ = *msg;
+    this->current_pose_ = *msg;
     }
 
   void StateMachine::UpdateState()
@@ -134,11 +94,10 @@ namespace statemachine
     {
       // Arming sequence
       if (current_state_.armed){
-        // Switch to TAKEOFF
         ROS_INFO("[ARMED] : Press ENTER for Take-off: ");
-        if(getInput() == 0)
+        if(getInput() == 0) 
             state_ = TAKEOFF;
-        requestKeyboardInput();       
+        requestKeyboardInput();
       } else {
         ROS_INFO("[ARMED] : Arming ...");
         setInput(100);
@@ -148,9 +107,9 @@ namespace statemachine
 
     case StateMachine::TAKEOFF:
     {
-      // TODO add velocity convergence
-      // TODO add heading convergence
-     
+      // Climb to a user defined safe altitude at the current position
+      // Once the altitude is reached go to the take off location
+
       bool arrivedWP = false;
       arrivedWP = WPConvergence( current_pose_, takeoff_pose_);
       
@@ -168,18 +127,27 @@ namespace statemachine
 
     case StateMachine::WAYPOINT:
     {
+      // Follow the waypoints in the YAML file 
+      // If the mission is completed ask the user to switch to next state
+      
       bool arrivedWP = false;
       arrivedWP = WPConvergence( current_pose_, des_pose_);
       
-      if (arrivedWP){
-        ROS_INFO("[WAYPOINT] : Waypoint reached!");
+      if(arrivedWP && current_wp_ < n_wp_){
+        ROS_INFO("[WAYPOINT] Arrived at WP %d!", current_wp_);
         if(getInput() == 0)
-          state_ = TRAJECTORY;
-          requestKeyboardInput(); 
+            current_wp_++;
+        requestKeyboardInput();
+      } else if (arrivedWP && (current_wp_ == n_wp_)){ //n_wp_-1
+        ROS_INFO("[WAYPOINT] Mission completed!");
+        if(getInput() == 0)
+          state_ = LANDING;
+        requestKeyboardInput();
       } else {
-        ROS_INFO("[WAYPOINT] : Fyling to WP ...");
+        ROS_INFO("[WAYPOINT] Flying to WP %d ...", current_wp_);
         setInput(100);
-        }
+      }
+      
       }
     break;
 
@@ -202,13 +170,15 @@ namespace statemachine
     
     case StateMachine::LANDING:
     {
-      // If the system status is not MAV_STATE_ACTIVE
+      // Lands at the current position
+
+      // If the system status is MAV_STATE_ACTIVE
       if (current_state_.system_status == 4){
         ROS_INFO("[LANDING] : Going down ...");
       } else {
         ROS_INFO("[LANDING] : Landing completed!");
         }
-      
+
       }
     break;
     
@@ -225,10 +195,10 @@ namespace statemachine
         
       local_pos_pub_.publish(des_pose_);
 
-      // Take-of preparation
-      this -> localtakeoff_pose_.pose.position.x = current_pose_.pose.position.x;
-      this -> localtakeoff_pose_.pose.position.y = current_pose_.pose.position.y;
-      this -> localtakeoff_pose_.pose.position.z = TAKEOFF_ALTITUDE;
+      // Take-off preparation by grabbing the current pose and target altitude
+      localtakeoff_pose_.pose.position.x = current_pose_.pose.position.x;
+      localtakeoff_pose_.pose.position.y = current_pose_.pose.position.y;
+      localtakeoff_pose_.pose.position.z = takeoff_pose_.pose.position.z;
         
       }
     break;
@@ -236,34 +206,41 @@ namespace statemachine
     case StateMachine::TAKEOFF:
     {
     
-      // Go to Local Origin at take-off height
-      if (abs(current_pose_.pose.position.z - TAKEOFF_ALTITUDE) < 0.10 ){
+      // Go to local origin at take-off height
+      if (abs(current_pose_.pose.position.z - takeoff_pose_.pose.position.z) < 0.10 ){ 
         local_pos_pub_.publish(takeoff_pose_);
-        } else {
-          // Climb to take-off altitude at current position
-          local_pos_pub_.publish(localtakeoff_pose_);
+      } else {
+        // Climb to the take-off altitude at current position
+        local_pos_pub_.publish(localtakeoff_pose_);
         }
-      
+    
       }
     break;
 
     case StateMachine::WAYPOINT:
-    {
-      this -> des_pose_.pose.position.x = 0;
-      this -> des_pose_.pose.position.y = 1;
-      this -> des_pose_.pose.position.z = 2.5;
+    { 
+      // Publish from waypoint matrix till all waypoints are completed
+      if (current_wp_ < n_wp_){
+        des_pose_.pose.position.x = wp_matrix_(0, current_wp_);
+        des_pose_.pose.position.y = wp_matrix_(1, current_wp_);
+        des_pose_.pose.position.z = wp_matrix_(2, current_wp_);
+      }
       
       local_pos_pub_.publish(des_pose_);
-
+      
       }
     break;
 
     case StateMachine::TRAJECTORY:
     {
+      // TODO Add basic trajectory generation; minimmum snap
+      
+      /*
       this -> des_pose_.pose.position.x = 1;
       this -> des_pose_.pose.position.y = 1;
       this -> des_pose_.pose.position.z = 2.5;
-
+      */
+      
       local_pos_pub_.publish(des_pose_);
       
       }
@@ -271,31 +248,170 @@ namespace statemachine
 
     case StateMachine::LANDING:
     {
-      //local_pos_pub_.publish(takeoff_pose_);
+      // Call the mavros landing service
       if (!land_cmd_.response.success){
         land_client_.call(land_cmd_);
         }
+     
       }
     break;
+    
     }
+  }
+
+  void StateMachine::ReadWaypoints()
+  {
+    // Read the waypoints from YAML file
+    double pos_x_wp;
+    double pos_y_wp;
+    double pos_z_wp;
+    double pos_h_wp;
+    bool no_fail = true;
+
+    ROS_INFO("[INIT]: Reading waypoints ...");
+    while(ros::ok() && no_fail){
+      
+      std::string idx = "/"+ std::to_string(n_wp_) + "/";
+      std::string wp_str = ros::this_node::getName() + "/waypoints" + idx;
+
+      //std::cout << wp_str << std::endl;
+      no_fail = nh_.getParam(wp_str + "pos/x", pos_x_wp);
+
+      if (!no_fail){
+        //std::cout << "Number of waypoints: " << n_wp_ << std::endl;
+        ROS_INFO("[INIT]: Number of waypoints: %d", n_wp_);  
+        break;
+      }else{ 
+        n_wp_++;
+      }
+    }
+
+    // Init Eige::Matrix4Xd
+    wp_matrix_.resize(4, n_wp_);
+    
+    for (int i = 0; i < n_wp_; i++){
+      
+      std::string idx = "/"+ std::to_string(i) + "/";
+      std::string wp_str = ros::this_node::getName() + "/waypoints" + idx;
+
+      no_fail = nh_.getParam(wp_str + "pos/x", pos_x_wp);
+      no_fail = nh_.getParam(wp_str + "pos/y", pos_y_wp);
+      no_fail = nh_.getParam(wp_str + "pos/z", pos_z_wp);
+      no_fail = nh_.getParam(wp_str + "pos/h", pos_h_wp);
+      
+      if(no_fail){
+        wp_matrix_.col(i) << pos_x_wp, pos_y_wp, pos_z_wp, pos_h_wp;
+      }else{
+        ROS_ERROR("[INIT]: Reading waypoints failed!");
+          
+      }
+    }
+    
+    // // To visualize the WP Matrix
+    // std::stringstream ss;
+    // ss << wp_matrix_;
+    // std::cout << ss.str() << "\n" << std::endl;
+
+    ROS_INFO("[INIT]: Waypoints ready");
+
+  }
+
+  void StateMachine::InitializeFCU()
+  {
+    // FCU Initialisation Sequence 
+    ros::Time last_request = ros::Time::now();
+
+    // Wait for FCU connection before publishing anything
+    while(ros::ok() && !current_state_.connected && 
+    (ros::Time::now() - last_request > ros::Duration(5.0))){
+      ros::spinOnce();
+      rate_.sleep();
+      ROS_INFO("[INIT]: Connecting to FCT ...");
+    }
+    
+    // Send a few setpoints before starting, 
+    // Otherwise cant switch to Offboard
+    for(int i = 100; ros::ok() && i > 0; --i){
+      local_pos_pub_.publish(des_pose_);
+      ros::spinOnce();
+      rate_.sleep();
+    }
+
+    // Timer used to prevent FCU overflowing
+    //ros::Time last_request = ros::Time::now();
+    last_request = ros::Time::now();
+        
+    local_pos_pub_.publish(des_pose_);
+
+    // Change to Offboard mode and arm w. time gaps
+    while(ros::ok() && !current_state_.armed){
+      if( current_state_.mode != "OFFBOARD" && 
+      (ros::Time::now() - last_request > ros::Duration(FCU_TIME_BUFFER))){
+          if( set_mode_client_.call(offb_mode_cmd_) && offb_mode_cmd_.response.mode_sent){
+              ROS_INFO("[INIT]: Offboard enabled");
+          }
+          last_request = ros::Time::now();
+      } else {
+          if( current_state_.mode == "OFFBOARD" && !current_state_.armed &&
+              (ros::Time::now() - last_request > ros::Duration(FCU_TIME_BUFFER))){
+              if( arming_client_.call(arm_cmd_) &&
+                  arm_cmd_.response.success){
+                  ROS_INFO("[INIT]: Vehicle armed");
+              }
+              last_request = ros::Time::now();
+          }
+      }
+      local_pos_pub_.publish(des_pose_);
+      ros::spinOnce();
+      rate_.sleep();
+    }   
+  }
+
+  void StateMachine::InitializePublishers()
+  {
+    local_pos_pub_ = nh_.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 1);
+  }
+
+  void StateMachine::InitializeSubscribers()
+  {
+    state_sub_ = nh_.subscribe<mavros_msgs::State>
+            ("mavros/state", 10, &StateMachine::stateCallback, this);
+
+    local_pos_pose_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>
+            ("mavros/local_position/pose", 10, &StateMachine::localposeCallback, this);
+  }
+
+  void StateMachine::InitializeServices()
+  {
+    arming_client_ = nh_.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/arming");
+
+    set_mode_client_ = nh_.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode");
+
+    land_client_ = nh_.serviceClient<mavros_msgs::CommandTOL>
+            ("mavros/cmd/land");
   }
 
   bool StateMachine::WPConvergence(geometry_msgs::PoseStamped pose1, geometry_msgs::PoseStamped pose2)
   {
-      Vec3 v1;
-      Vec3 v2;
+    // TODO : Add velocity and orientation checks
+    
+    Vec3 v1;
+    Vec3 v2;
 
-      v1(0) = pose1.pose.position.x;
-      v1(1) = pose1.pose.position.y;
-      v1(2) = pose1.pose.position.z;
+    v1(0) = pose1.pose.position.x;
+    v1(1) = pose1.pose.position.y;
+    v1(2) = pose1.pose.position.z;
 
-      v2(0) = pose2.pose.position.x;
-      v2(1) = pose2.pose.position.y;
-      v2(2) = pose2.pose.position.z;
+    v2(0) = pose2.pose.position.x;
+    v2(1) = pose2.pose.position.y;
+    v2(2) = pose2.pose.position.z;
 
-      bool converged = (v1 - v2).norm() < 0.10;
+    bool converged = (v1 - v2).norm() < 0.10;
 
-      return converged;
+    return converged;
   }
 
   void StateMachine::requestKeyboardInput()
